ff6b9a6fb259e27ceea0b4062d9296019e98ef49
David Lignell <dol4@ash.et.byu.edu>
2021-11-02 11:15:29 -0600

    Lots of changes; cmp completion

--- chromerefresh.sh
+++ chromerefresh.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # DOL; found this online. Using with reveal.js for ChEn 433 lecture notes.
+# http://loopduplicate.com/content/refresh-chrome-from-the-mac-terminal-using-applescript
 # See also init.vim markdown area remap leader v to save, compile code and refresh chrome (here)
 
 exec <"$0" || exit; read v; read v; exec /usr/bin/osascript - "$@"; exit
